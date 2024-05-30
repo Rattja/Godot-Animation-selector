11482f5c6064a9dd4a7a1e213e75356c2d29a45f
Rattja <99655927+Rattja@users.noreply.github.com>
2024-05-30 16:19:22 +0200

    Update treefinder.gd

--- addons/animationselect/treefinder.gd
+++ addons/animationselect/treefinder.gd
@@ -3,11 +3,9 @@ class_name AnimationTreeFinder
 
 ## Finds the animation tree that is relevant for the given node.[br]
 ## This method must be modified to fit the given project structure.[br]
-static func _set_animation_tree(node:Node) -> AnimationTree:
-	var animator = node.owner.get_component(AnimatorComponent)
-	var animation_tree : AnimationTree = animator.anim_tree
-	if !animator:
-		return null
+static func set_animation_tree(node:Node) -> AnimationTree:
+	# Example:
+	var animation_tree : AnimationTree = node.animation_tree
 	if !animation_tree:
 		return null
 	return animation_tree
